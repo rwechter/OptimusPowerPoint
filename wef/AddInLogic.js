04ae6cb34f34dfcc43a573f62a6506e4bcedbde1
Ryan Wechter <RyanWechter@Ryans-MacBook-Pro.local>
2017-01-31 12:05:20 -0800

    Copied UI,Logic, and Manifest files into repo

---
+++ wef/AddInLogic.js
@@ -0,0 +1,21 @@
+// The initialize function is required for all add-ins.
+Office.initialize = function (reason) {
+
+    // Checks for the DOM to load using the jQuery ready function.
+    $(document).ready(function () {
+
+        // Execute sendFile when submit is clicked
+        $('#submit').click(function () {
+            sendFile();
+        });
+
+        // Update status
+        updateStatus("Ready to send file.");
+    });
+}
+
+// Create a function for writing to the status div.
+function updateStatus(message) {
+    var statusInfo = $('#status');
+    statusInfo.innerHTML += message + "<br/>";
+}
